165d29efa7c474484576f4ee02788ea1f140825c
Joan Boulangger <joan.boulangger@gmail.com>
2020-06-03 18:30:04 -0500

    Updates

---
+++ precache-manifest.102adf60188ea940ce7aff2a5a61c04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80da39b2f0d8eb0cb6ef33235c56e88a",
+    "url": "/React-Generador-de-horario/index.html"
+  },
+  {
+    "revision": "73d6aea3c6c6e1a43aaf",
+    "url": "/React-Generador-de-horario/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "73d6aea3c6c6e1a43aaf",
+    "url": "/React-Generador-de-horario/static/js/2.e218eba2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Generador-de-horario/static/js/2.e218eba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96084298bcea8413e2c9",
+    "url": "/React-Generador-de-horario/static/js/main.a84bd014.chunk.js"
+  },
+  {
+    "revision": "3da321eac92416704672",
+    "url": "/React-Generador-de-horario/static/js/runtime-main.8097942c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Generador-de-horario/precache-manifest.102adf60188ea940ce7aff2a5a61c04e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Generador-de-horario/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
